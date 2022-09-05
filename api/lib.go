f966c8fd2323656becbfb37239dc2c778a44cdd0
Geoff Lee <Vritra@hitel.net>
2022-09-05 13:02:33 +0900

    add stub

---
+++ api/lib.go
@@ -0,0 +1,20 @@
+package api
+
+// #include <stdlib.h>
+// #include "bindings.h"
+import "C"
+
+// Value types
+type (
+	cint   = C.int
+	cbool  = C.bool
+	cusize = C.size_t
+	cu8    = C.uint8_t
+	cu32   = C.uint32_t
+	cu64   = C.uint64_t
+	ci8    = C.int8_t
+	ci32   = C.int32_t
+	ci64   = C.int64_t
+	cu8_ptr = *C.uint8_t
+)
+
