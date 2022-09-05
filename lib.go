6166993d0ba721a6d2ea2439d742f87c365c4896
Geoff Lee <Vritra@hitel.net>
2022-09-05 10:45:38 +0900

    add stub VM

---
+++ lib.go
@@ -0,0 +1,25 @@
+package kernel
+
+// VM struct is the core of kernelvm.
+type VM struct {
+}
+
+// CreateVM creates a new VM.
+// TODO: add params and returns
+func CreateVM() {}
+
+// VM Destroyer
+// TODO: add params and returns
+func (vm *VM) Destroy() {}
+
+// PublishModule will publish a given module.
+// TODO: add params and returns
+func (vm *VM) PublishModule() {}
+
+// Query will do a query request to VM
+// TODO: add params and returns
+func (vm *VM) Query() {}
+
+// Execute calls a given contract.
+// TODO: add params and returns
+func (vm *VM) Execute() {}
