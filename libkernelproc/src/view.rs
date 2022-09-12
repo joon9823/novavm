42c559fa7c93c3f96e7b06953f23f70afe231743
Geoff Lee <Vritra@hitel.net>
2022-09-12 17:42:09 +0900

    add CosmosView::get()

--- libkernelproc/src/view.rs
+++ libkernelproc/src/view.rs
@@ -1,9 +1,10 @@
 
 
 // this crate handles communication between db.rs and vm.rs
-use crate::Db;
+use crate::{Db, UnmanagedVector, U8SliceView, error::GoError};
 
 use kernelvm::vm::{storage::state_view::StateView, access_path::AccessPath};
+use anyhow::{anyhow, Error};
 
 pub struct CosmosView {
     db: Db,
@@ -17,6 +18,37 @@ impl CosmosView {
 
 impl StateView for CosmosView {
     fn get(&self, access_path: &AccessPath) -> anyhow::Result<Option<Vec<u8>>> {
-        anyhow::Result::Ok(None) // FIXME: just stub
+        let key = access_path.to_string(); // FIXME: replace to_string to to_cosmos_key
+        let mut output = UnmanagedVector::default();
+        let mut error_msg = UnmanagedVector::default();
+        let mut used_gas = 0_u64;
+        let go_error: GoError = (self.db.vtable.read_db)(
+            self.db.state,
+            self.db.gas_meter,
+            &mut used_gas as *mut u64,
+            U8SliceView::new(Some(key.as_bytes())),
+            &mut output as *mut UnmanagedVector,
+            &mut error_msg as *mut UnmanagedVector,
+        )
+        .into();
+        // We destruct the UnmanagedVector here, no matter if we need the data.
+        let output = output.consume();
+
+        // FIXME: uncomment: let gas_info = GasInfo::with_externally_used(used_gas);
+
+        // return complete error message (reading from buffer for GoError::Other)
+        let default = || {
+            format!(
+                "Failed to read a key in the db: {}",
+                String::from_utf8_lossy(key.as_bytes())
+            )
+        };
+        unsafe {
+            if let Err(err) = go_error.into_result(error_msg, default) {
+                return Err(anyhow!(err))
+            }
+        }
+
+        anyhow::Result::Ok(output/* , gas_info*/) //FIXME: add gas_info?
     }
 }
