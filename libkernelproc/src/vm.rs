b0c675e6defd0af7d9a2acb28955ff7a6b0a0378
Geoff Lee <Vritra@hitel.net>
2022-09-15 12:19:35 +0900

    [wip] impl vm

--- libkernelproc/src/vm.rs
+++ libkernelproc/src/vm.rs
@@ -2,11 +2,12 @@ use crate::Db;
 use crate::view::CosmosView;
 use crate::error::Error;
 
+use kernelvm::vm::kernel_vm::VMStatus;
 use move_deps::move_core_types::{account_address::AccountAddress};
 use kernelvm::EntryFunction;
 use kernelvm::Module;
 use kernelvm::vm::storage::data_view_resolver::DataViewResolver;
-use kernelvm::vm::kernel_vm::KernelVM;
+use kernelvm::vm::kernel_vm::{KernelVM};
 use kernelvm::Message;
 use kernelvm::gas_meter::Gas;
 
@@ -19,9 +20,14 @@ pub(crate) fn initialize_vm(module_bundle: Vec<u8>, db_handle: Db) -> Result<Vec
 	let data_view = DataViewResolver::new(&cv);
 
     let (status, output, retval) = unsafe { INSTANCE.initialize(module_bundle, &data_view) }.unwrap();
-
     // TODO handle results
-    Ok(Vec::from("PLACEHOLDER"))
+
+    let ok = Vec::from(status.to_string()); // FIXME: IT IS JUST PLACEHOLDER
+
+    match status {
+        VMStatus::Executed => { Ok(ok) },
+        _ => { Err(Error::vm_err("failed to intitialize"))}
+    }
 }
 
 pub(crate) fn publish_module(sender: AccountAddress, payload: Vec<u8>, db_handle: Db, gas: u64) -> Result<Vec<u8>, Error>{
@@ -36,7 +42,12 @@ pub(crate) fn publish_module(sender: AccountAddress, payload: Vec<u8>, db_handle
     let (status, output, retval) = unsafe { INSTANCE.execute_message(message, &data_view, gas_limit) };
     // TODO handle results
 
-    Ok(Vec::from("PLACEHOLDER"))
+    let ok = Vec::from(status.to_string()); // FIXME: IT IS JUST PLACEHOLDER
+
+    match status {
+        VMStatus::Executed => { Ok(ok) },
+        _ => { Err(Error::vm_err("failed to intitialize"))}
+    }
 }
 
 pub(crate) fn execute_contract(sender: AccountAddress, payload: Vec<u8>, db_handle: Db, gas: u64) -> Result<Vec<u8>, Error> {
@@ -51,5 +62,11 @@ pub(crate) fn execute_contract(sender: AccountAddress, payload: Vec<u8>, db_hand
 
     let (status, output, retval) = unsafe { INSTANCE.execute_message(message, &data_view, gas_limit) };
     // TODO handle results
-    Ok(Vec::from("PLACEHOLDER"))
+    
+    let ok = Vec::from(status.to_string()); // FIXME: IT IS JUST PLACEHOLDER
+
+    match status {
+        VMStatus::Executed => { Ok(ok) },
+        _ => { Err(Error::vm_err("failed to intitialize"))}
+    }
 }
