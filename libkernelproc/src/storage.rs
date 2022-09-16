a3205d8ddfa68123f3f579f759f916458ef971e2
Geoff Lee <Vritra@hitel.net>
2022-09-16 15:08:47 +0900

    remove db-related gas perfusion from libkernelproc

--- libkernelproc/src/storage.rs
+++ libkernelproc/src/storage.rs
@@ -3,7 +3,7 @@
 
 use kernelvm::access_path::AccessPath;
 //use kernelvm::BackendError;
-use kernelvm::backend::{BackendResult, GasInfo};
+use kernelvm::backend::BackendResult;
 use kernelvm::storage::state_view::StateView;
 
 use crate::db::Db;
@@ -23,36 +23,6 @@ pub trait Storage {
     /// is not great yet and might not be possible in all backends. But we're trying to get there.
     fn get(&self, key: &[u8]) -> BackendResult<Option<Vec<u8>>>;
 
-    /* we don't need iterators
-    /// Allows iteration over a set of key/value pairs, either forwards or backwards.
-    /// Returns an interator ID that is unique within the Storage instance.
-    ///
-    /// The bound `start` is inclusive and `end` is exclusive.
-    ///
-    /// If `start` is lexicographically greater than or equal to `end`, an empty range is described, mo matter of the order.
-    ///
-    /// This call must not change data in the storage, but creating and storing a new iterator can be a mutating operation on
-    /// the Storage implementation.
-    /// The implementation must ensure that iterator IDs are assigned in a deterministic manner as this is
-    /// environment data that is injected into the contract.
-    #[cfg(feature = "iterator")]
-    fn scan(
-        &mut self,
-        start: Option<&[u8]>,
-        end: Option<&[u8]>,
-        order: Order,
-    ) -> BackendResult<u32>;
-
-    /// Returns the next element of the iterator with the given ID.
-    ///
-    /// If the ID is not found, a BackendError::IteratorDoesNotExist is returned.
-    ///
-    /// This call must not change data in the storage, but incrementing an iterator can be a mutating operation on
-    /// the Storage implementation.
-    #[cfg(feature = "iterator")]
-    fn next(&mut self, iterator_id: u32) -> BackendResult<Option<Record>>;
-    */
-
     fn set(&mut self, key: &[u8], value: &[u8]) -> BackendResult<()>;
 
     /// Removes a database entry at `key`.
@@ -64,14 +34,12 @@ pub trait Storage {
 
 pub struct GoStorage {
     db: Db,
-    //iterators: HashMap<u32, GoIter>,
 }
 
 impl GoStorage {
     pub fn new(db: Db) -> Self {
         GoStorage {
             db,
-            //iterators: HashMap::new(),
         }
     }
 }
@@ -81,11 +49,8 @@ impl StateView for GoStorage {
         let key = access_path.to_string(); // FIXME: replace to_string to to_cosmos_key
         let mut output = UnmanagedVector::default();
         let mut error_msg = UnmanagedVector::default();
-        let mut used_gas = 0_u64;
         let go_error: GoError = (self.db.vtable.read_db)(
             self.db.state,
-            self.db.gas_meter,
-            &mut used_gas as *mut u64,
             U8SliceView::new(Some(key.as_bytes())),
             &mut output as *mut UnmanagedVector,
             &mut error_msg as *mut UnmanagedVector,
@@ -93,8 +58,6 @@ impl StateView for GoStorage {
         .into();
         // We destruct the UnmanagedVector here, no matter if we need the data.
         let output = output.consume();
-
-        // FIXME: uncomment: let gas_info = GasInfo::with_externally_used(used_gas);
 
         // return complete error message (reading from buffer for GoError::Other)
         let default = || {
@@ -109,7 +72,7 @@ impl StateView for GoStorage {
             }
         }
 
-        anyhow::Result::Ok(output/* , gas_info*/) //FIXME: add gas_info?
+        anyhow::Result::Ok(output)
     }
 }
 
@@ -117,11 +80,8 @@ impl Storage for GoStorage {
     fn get(&self, key: &[u8]) -> BackendResult<Option<Vec<u8>>> {
         let mut output = UnmanagedVector::default();
         let mut error_msg = UnmanagedVector::default();
-        let mut used_gas = 0_u64;
         let go_error: GoError = (self.db.vtable.read_db)(
             self.db.state,
-            self.db.gas_meter,
-            &mut used_gas as *mut u64,
             U8SliceView::new(Some(key)),
             &mut output as *mut UnmanagedVector,
             &mut error_msg as *mut UnmanagedVector,
@@ -129,8 +89,6 @@ impl Storage for GoStorage {
         .into();
         // We destruct the UnmanagedVector here, no matter if we need the data.
         let output = output.consume();
-
-        let gas_info = GasInfo::with_externally_used(used_gas);
 
         // return complete error message (reading from buffer for GoError::Other)
         let default = || {
@@ -141,26 +99,22 @@ impl Storage for GoStorage {
         };
         unsafe {
             if let Err(err) = go_error.into_result(error_msg, default) {
-                return (Err(err), gas_info);
+                return Err(err);
             }
         }
 
-        (Ok(output), gas_info)
+        Ok(output)
     }
 
     fn set(&mut self, key: &[u8], value: &[u8]) -> BackendResult<()> {
         let mut error_msg = UnmanagedVector::default();
-        let mut used_gas = 0_u64;
         let go_error: GoError = (self.db.vtable.write_db)(
             self.db.state,
-            self.db.gas_meter,
-            &mut used_gas as *mut u64,
             U8SliceView::new(Some(key)),
             U8SliceView::new(Some(value)),
             &mut error_msg as *mut UnmanagedVector,
         )
         .into();
-        let gas_info = GasInfo::with_externally_used(used_gas);
         // return complete error message (reading from buffer for GoError::Other)
         let default = || {
             format!(
@@ -170,24 +124,20 @@ impl Storage for GoStorage {
         };
         unsafe {
             if let Err(err) = go_error.into_result(error_msg, default) {
-                return (Err(err), gas_info);
+                return Err(err);
             }
         }
-        (Ok(()), gas_info)
+        Ok(())
     }
 
     fn remove(&mut self, key: &[u8]) -> BackendResult<()> {
         let mut error_msg = UnmanagedVector::default();
-        let mut used_gas = 0_u64;
         let go_error: GoError = (self.db.vtable.remove_db)(
             self.db.state,
-            self.db.gas_meter,
-            &mut used_gas as *mut u64,
             U8SliceView::new(Some(key)),
             &mut error_msg as *mut UnmanagedVector,
         )
         .into();
-        let gas_info = GasInfo::with_externally_used(used_gas);
         let default = || {
             format!(
                 "Failed to delete a key in the db: {}",
@@ -196,9 +146,9 @@ impl Storage for GoStorage {
         };
         unsafe {
             if let Err(err) = go_error.into_result(error_msg, default) {
-                return (Err(err), gas_info);
+                return Err(err);
             }
         }
-        (Ok(()), gas_info)
+        Ok(())
     }
 }
--- vm/src/backend.rs
+++ vm/src/backend.rs
@@ -1,5 +1,3 @@
-use std::ops::AddAssign;
-
 use crate::errors::BackendError;
 use crate::storage::state_view::StateView;
 
@@ -16,64 +14,7 @@ pub struct Backend<A: BackendApi, S: StateView, Q: Querier> {
 /// A result type for calling into the backend. Such a call can cause
 /// non-negligible computational cost in both success and faiure case and must always have gas information
 /// attached.
-pub type BackendResult<T> = (core::result::Result<T, BackendError>, GasInfo);
-
-/// A structure that represents gas cost to be deducted from the remaining gas.
-/// This is always needed when computations are performed outside of
-/// Wasm execution, such as calling crypto APIs or calls into the blockchain.
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub struct GasInfo {
-    /// The gas cost of a computation that was executed already but not yet charged.
-    ///
-    /// This could be renamed to `internally_used` for consistency because it is used inside
-    /// of the `kernelvm`.
-    pub cost: u64,
-    /// Gas that was used and charged externally. This is needed to
-    /// adjust the VM's gas limit but does not affect the gas usage.
-    pub externally_used: u64,
-}
-
-impl GasInfo {
-    pub fn new(cost: u64, externally_used: u64) -> Self {
-        GasInfo {
-            cost,
-            externally_used,
-        }
-    }
-
-    pub fn with_cost(amount: u64) -> Self {
-        GasInfo {
-            cost: amount,
-            externally_used: 0,
-        }
-    }
-
-    pub fn with_externally_used(amount: u64) -> Self {
-        GasInfo {
-            cost: 0,
-            externally_used: amount,
-        }
-    }
-
-    /// Creates a gas information with no cost for the caller and with zero externally used gas.
-    ///
-    /// Caution: when using this you need to make sure no gas was metered externally to keep the gas values in sync.
-    pub fn free() -> Self {
-        GasInfo {
-            cost: 0,
-            externally_used: 0,
-        }
-    }
-}
-
-impl AddAssign for GasInfo {
-    fn add_assign(&mut self, other: Self) {
-        *self = GasInfo {
-            cost: self.cost + other.cost,
-            externally_used: self.externally_used + other.externally_used,
-        };
-    }
-}
+pub type BackendResult<T> = core::result::Result<T, BackendError>;
 
 /// Callbacks to system functions defined outside of the wasm modules.
 /// This is a trait to allow Mocks in the test code.
@@ -101,6 +42,5 @@ pub trait Querier {
     fn query_raw(
         &self,
         request: &[u8],
-        gas_limit: u64,
     ) -> BackendResult<Vec<u8>>; /* FIXME : put Vec<u8> as stub */
 }
