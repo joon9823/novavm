16104487e5e66f0d25da71c9e6e58aec88509917
JSHan <saw151515@gmail.com>
2022-09-19 19:42:53 +0900

    fix gas currency unit

--- vm/src/gas/algebra.rs
+++ vm/src/gas/algebra.rs
@@ -7,14 +7,13 @@ pub use crate::gas::gas_algebra_ext::{
 /// Unit of (external) gas.
 pub enum GasUnit {}
 
-// TODO: what will be our unit?
-/// Unit of gas currency. 1 Octa = 10^-8 Aptos coins.
-pub enum Octa {}
+/// Unit of gas currency. 1 Hexa = 10^-6 coins.
+pub enum Hexa {}
 
 pub type Gas = GasQuantity<GasUnit>;
 
 pub type GasScalingFactor = GasQuantity<UnitDiv<InternalGasUnit, GasUnit>>;
 
-pub type Fee = GasQuantity<Octa>;
+pub type Fee = GasQuantity<Hexa>;
 
-pub type FeePerGasUnit = GasQuantity<UnitDiv<Octa, GasUnit>>;
+pub type FeePerGasUnit = GasQuantity<UnitDiv<Hexa, GasUnit>>;
