e00680459ced6ea76814356b967346743e523f06
Jeff <jeffjoin@gmail.com>
2022-10-13 02:54:10 +0900

    fix compiler has no native context

--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,13 +1,15 @@
 #![cfg_attr(feature = "backtraces", feature(backtrace))]
 #![allow(clippy::not_unsafe_ptr_arg_deref, clippy::missing_safety_doc)]
 
-pub mod compiler;
 pub mod clean;
+pub mod compiler;
 pub mod new;
 
-pub use compiler::{compile, Command};
 pub use clean::Clean;
+pub use compiler::{compile, Command};
 pub use new::New;
+
+mod mock;
 
 #[cfg(test)]
 mod tests;
